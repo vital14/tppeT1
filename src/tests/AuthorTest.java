c0106a3328a9a13294adf30d94e7ecc4d290aab6
VitorLamego <vitorlamegoo@gmail.com>
2023-05-23 19:02:10 -0300

    Parametrizando método de Multi Campos

--- src/tests/AuthorTest.java
+++ src/tests/AuthorTest.java
@@ -21,11 +21,13 @@ public class AuthorTest {
 	private Author author;
 	private boolean resultExpected;
 	private boolean resultExpectedOrInclusive;
+	private float resultMultiFields;
 	
 	public AuthorTest(HashMap<String, Object> data) {
 		this.author = new Author((String)data.get("nationality"), (String)data.get("birthCountry"), (String)data.get("birthCity"), (String)data.get("birthState"), (String)data.get("identifierLattes"), (String)data.get("identifierOrcId"));
 		this.resultExpected = (boolean)data.get("result");
 		this.resultExpectedOrInclusive = (boolean)data.get("resultOrInclusive");
+		this.resultMultiFields = (float)data.get("completeness");
 	}
 	
 	@Parameters
@@ -42,39 +44,41 @@ public class AuthorTest {
 		authorComplete.put("identifierOrcId", "23232");
 		authorComplete.put("result", false);
 		authorComplete.put("resultOrInclusive", true);
+		authorComplete.put("completeness", 0.5f);
 
 		
 		parameters.add(authorComplete);
-		
-		HashMap<String, Object> authorWithoutIdentifierAndNationality = new HashMap<String, Object>();
-		
-		authorWithoutIdentifierAndNationality.put("birthCountry", "Brasil");
-		authorWithoutIdentifierAndNationality.put("birthCity", "Brasília");
-		authorWithoutIdentifierAndNationality.put("birthState", "DF");
-		authorWithoutIdentifierAndNationality.put("result", false);
-		authorWithoutIdentifierAndNationality.put("resultOrInclusive", true);
-		
-		parameters.add(authorWithoutIdentifierAndNationality);
 
 		HashMap<String, Object> authorWithOnlyOneIdentifier = new HashMap<String, Object>();
 		
 		authorWithOnlyOneIdentifier.put("identifierLattes", "89273823");
 		authorWithOnlyOneIdentifier.put("result", true);
 		authorWithOnlyOneIdentifier.put("resultOrInclusive", false);
+		authorWithOnlyOneIdentifier.put("completeness", 0.5f);
 		
 		parameters.add(authorWithOnlyOneIdentifier);
 		
-		HashMap<String, Object> authorWithoutNationality = new HashMap<String, Object>();
+		HashMap<String, Object> authorWithoutNationalityAndOneIdentifier = new HashMap<String, Object>();
 		
-		authorWithoutNationality.put("birthCountry", "Brasil");
-		authorWithoutNationality.put("birthCity", "Brasília");
-		authorWithoutNationality.put("birthState", "DF");
-		authorWithoutNationality.put("identifierLattes", "89273823");
-		authorWithoutNationality.put("identifierOrcId", "23232");
-		authorWithoutNationality.put("result", false);
-		authorWithoutNationality.put("resultOrInclusive", true);
+		authorWithoutNationalityAndOneIdentifier.put("birthCountry", "Brasil");
+		authorWithoutNationalityAndOneIdentifier.put("birthCity", "Brasília");
+		authorWithoutNationalityAndOneIdentifier.put("birthState", "DF");
+		authorWithoutNationalityAndOneIdentifier.put("identifierLattes", "89273823");
+		authorWithoutNationalityAndOneIdentifier.put("result", true);
+		authorWithoutNationalityAndOneIdentifier.put("resultOrInclusive", true);
+		authorWithoutNationalityAndOneIdentifier.put("completeness", 1f);
+
 		
-		parameters.add(authorWithoutNationality);
+		parameters.add(authorWithoutNationalityAndOneIdentifier);
+		
+		HashMap<String, Object> emptyAuthor = new HashMap<String, Object>();
+		
+		emptyAuthor.put("result", false);
+		emptyAuthor.put("resultOrInclusive", false);
+		emptyAuthor.put("completeness", 0f);
+
+		
+		parameters.add(emptyAuthor);
 		
 		
 		
@@ -84,12 +88,14 @@ public class AuthorTest {
 	@Test
 	public void testOrExclusivo() {
 		assertEquals(author.checkExclusiveOrFields(), resultExpected);
-
 	}
 	
 	@Test
 	public void testOrInclusivo() {
 		assertEquals(author.checkInclusiveOrFields(), resultExpectedOrInclusive);
-
+	}
+	
+	@Test public void testMultiFields() {
+		assertEquals(author.checkCompletenessMultiField(), resultMultiFields, 0f);
 	}
 }
